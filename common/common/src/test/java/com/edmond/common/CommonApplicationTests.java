1417eba2d42e4b5bcef10585963e7b61c2ffbc72
Suh-Edmond <suhedmond25@yahoo.com>
2025-02-19 15:59:03 +0100

    setup project

---
+++ common/common/src/test/java/com/edmond/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edmond.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ paymentservice/paymentservice/src/test/java/com/edmond/paymentservice/PaymentserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edmond.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
